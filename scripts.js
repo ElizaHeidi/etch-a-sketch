a8163f5bed80e35a06e177d97fe02393f9f0cd6d
Elizabeth Heideman <77503008+ElizaHeidi@users.noreply.github.com>
2023-03-20 14:53:38 -0500

    Add prompt and New Canvas button.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
  
 const container = document.querySelector('#container');
 const div = document.querySelector('div');
+const button = document.querySelector('button');
 
 // Create indiv div num times
 const makeDivs = function(numDivs) {
@@ -15,6 +16,11 @@ div.addEventListener('mouseover', function(e) {
     e.target.style.background = 'goldenrod';
 })
 
+// When clicked, button fires user prompt asks for number of squarws with max at 100.
+//prompt('Type how many squares you want your canvas to BeforeUnloadEvent. Enter no more than 100.', '100') 
+// button creates new grid with x squares in same 960px space.   
 
-
+const squares = function() {
+    prompt('Type how many squares you want your canvas to have. Enter no more than 100.', '100')
+}
    
