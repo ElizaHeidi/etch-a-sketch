907082645f8851ac100252d6d8244e44c92318ea
Elizabeth Heideman <77503008+ElizaHeidi@users.noreply.github.com>
2023-03-20 17:02:05 -0500

    New page button dynamically adjusts size based on user input.

--- scripts.js
+++ scripts.js
@@ -5,22 +5,31 @@ const button = document.querySelector('button');
 
 // Create indiv div num times
 const makeDivs = function(numDivs) {
+   // removes existing grid and starts fresh
+   while (container.hasChildNodes()) {
+    container.removeChild(container.lastChild)
+   }
+   // creates divs
     for (i = 0; i < numDivs; i++) {
         const div = document.createElement('div');
         container.appendChild(div);
 }
 }
-makeDivs(256);
-
 div.addEventListener('mouseover', function(e) {
     e.target.style.background = 'goldenrod';
 })
 
 // When clicked, button fires user prompt asks for number of squarws with max at 100.
-//prompt('Type how many squares you want your canvas to BeforeUnloadEvent. Enter no more than 100.', '100') 
 // button creates new grid with x squares in same 960px space.   
 
-const squares = function() {
-    prompt('Type how many squares you want your canvas to have. Enter no more than 100.', '100')
-}
+button.addEventListener('click', () => {
+let userSize = Number(prompt('Type how many squares you want your canvas to have. Enter no more than 100.', '100'))
+
+while (userSize >100 ) {
+    const userSize = Number(prompt('Number must be less than 100'));
+
+} makeDivs(userSize * userSize);
+})
+
+
    
