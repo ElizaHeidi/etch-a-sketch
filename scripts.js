25157d145f9eb2107356af88fd6dc7b33bbf9503
Elizabeth Heideman <77503008+ElizaHeidi@users.noreply.github.com>
2023-03-20 14:16:41 -0500

    Adjust size of cells, add mouseover color change.

--- scripts.js
+++ scripts.js
@@ -1,14 +1,20 @@
  
 const container = document.querySelector('#container');
-
+const div = document.querySelector('div');
 
 // Create indiv div num times
 const makeDivs = function(numDivs) {
     for (i = 0; i < numDivs; i++) {
-        let div = document.createElement('div');
+        const div = document.createElement('div');
         container.appendChild(div);
 }
 }
-
 makeDivs(256);
 
+div.addEventListener('mouseover', function(e) {
+    e.target.style.background = 'goldenrod';
+})
+
+
+
+   
