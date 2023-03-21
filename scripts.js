5980de2067a1829635490941766b63b15abfcfbe
Elizabeth Heideman <77503008+ElizaHeidi@users.noreply.github.com>
2023-03-20 19:10:47 -0500

    Change pixel size to 1fr so that they automatically fit to container

--- scripts.js
+++ scripts.js
@@ -3,28 +3,28 @@ const container = document.querySelector('#container');
 const div = document.querySelector('div');
 const button = document.querySelector('button');
 
-// Create indiv div num times
+
 const makeDivs = function(numDivs) {
    // removes existing grid and starts fresh
    while (container.hasChildNodes()) {
     container.removeChild(container.lastChild)
    }
-   // creates divs
+   // creates individual divs
     for (i = 0; i < numDivs; i++) {
         const div = document.createElement('div');
         container.appendChild(div);
 }
 }
+// Permanently changes div color on mouseover
 div.addEventListener('mouseover', function(e) {
     e.target.style.background = 'goldenrod';
 })
 
-// When clicked, button fires user prompt asks for number of squarws with max at 100.
-// button creates new grid with x squares in same 960px space.   
-
+// When clicked, button fires user prompt asks for number of squares with max at 100
 button.addEventListener('click', () => {
-let userSize = Number(prompt('Type how many squares you want your canvas to have. Enter no more than 100.', '100'))
+let userSize = Number(prompt('Type how many squares you want your canvas to have. Must be less than 100.', '42'))
 
+// If user inputs > 100, re-fires prompt with alt message 
 while (userSize >100 ) {
     const userSize = Number(prompt('Number must be less than 100'));
 
